ab3ed647e2ce6eddd10470a15afc339610147a0f
Zidan Herlangga <133259343+zidanherlangga04@users.noreply.github.com>
2023-05-15 19:56:38 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const webCamElement = document.getElementById("webCam");
 const canvasElement = document.getElementById("canvas");
 const webcam = new Webcam(webCamElement, "user", canvasElement);
+
+alert(`Desclaimer!
+Project ini tidak akan merekam aktivitas device Anda, termasuk indentitas Anda.`)
+
 webcam.start();
 
 function takeAPicture() {
